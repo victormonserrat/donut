a483ebd2beff8d4a235a3f7293a666f9e9a78c47
Victor Monserrat <victormonserratvillatoro@gmail.com>
2019-08-02 18:13:14 +0200

    Add donut command

---
+++ src/Application/Command/AddDonut.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+ * This file is part of the Donut package.
+ *
+ * (c) Victor Monserrat.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Donut\Application\Command;
+
+use Donut\Domain\Common\Model\Id;
+use Donut\Domain\Donut\Model\DonutTitle;
+
+final class AddDonut
+{
+    /** @var Id */
+    private $id;
+
+    /** @var DonutTitle */
+    private $title;
+
+    public static function with(Id $id, DonutTitle $title): self
+    {
+        $addDonut = new self();
+        $addDonut->id = $id;
+        $addDonut->title = $title;
+
+        return $addDonut;
+    }
+
+    public function id(): Id
+    {
+        return $this->id;
+    }
+
+    public function title(): DonutTitle
+    {
+        return $this->title;
+    }
+
+    private function __construct()
+    {
+    }
+}
